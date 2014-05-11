27a52988ae72fa8b879b271c6e6d2caa57c99ef3
Tangent 128 <Tangent128@gmail.com>
2014-05-11 16:44:09 -0400

    Parse <linux/input.h> for creating constants.lua file

---
+++ example/gen-constants.lua
@@ -0,0 +1,50 @@
+-- script for generating constants.lua from the linux/input.h header
+
+local capture = {
+	{"Event Types", "^EV_"},
+	{"Keys", "^KEY_"},
+	{"Buttons", "^BTN_"},
+	{"Relative Axes", "^REL_"},
+	{"Absolute Axes", "^ABS_"},
+}
+
+local header = [[
+
+local _ENV = {}
+
+-- Constants for the Linux evdev API
+-- created by the gen-constants.lua script from the <linux/input.h> header
+]]
+
+local footer = [[
+return _ENV
+]]
+
+local headerFile = ...
+
+if not headerFile then
+	print "Please provide the header file path as an argument"
+	return 1
+end
+
+local sections = {}
+for _, record in pairs(capture) do
+	sections[record[1]] = "-- " .. record[1] .. "\n"
+end
+
+for line in io.lines(headerFile) do
+	local name, value = line:match "^#define%s+(%S+)%s+(%S+)"
+	if name then
+		for _, record in pairs(capture) do
+			if name:match(record[2]) then
+				sections[record[1]] = sections[record[1]] .. name .. " = " .. value .. "\n"
+			end
+		end
+	end
+end
+
+print(header)
+for _, record in pairs(capture) do
+	print(sections[record[1]])
+end
+print(footer)
